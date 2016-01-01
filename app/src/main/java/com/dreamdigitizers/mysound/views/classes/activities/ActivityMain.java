18abcd9657ceb5e778426ddc6eb5c5d57b56c7dc
daole <lechanhdao240688@gmail.com>
2016-01-01 17:19:14 +0700

    navigation drawer

--- app/src/main/java/com/dreamdigitizers/mysound/views/classes/activities/ActivityMain.java
+++ app/src/main/java/com/dreamdigitizers/mysound/views/classes/activities/ActivityMain.java
@@ -1,11 +1,14 @@
 package com.dreamdigitizers.mysound.views.classes.activities;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.dreamdigitizers.androidbaselibrary.views.classes.activities.ActivityBase;
 import com.dreamdigitizers.androidbaselibrary.views.classes.fragments.screens.ScreenBase;
@@ -13,12 +16,13 @@ import com.dreamdigitizers.mysound.R;
 import com.dreamdigitizers.mysound.views.classes.fragments.screens.ScreenHome;
 
 public class ActivityMain extends ActivityBase {
-    private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private NavigationView mNavigationView;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu__main, menu);
+        this.getMenuInflater().inflate(R.menu.menu__main, menu);
         return true;
     }
 
@@ -26,9 +30,9 @@ public class ActivityMain extends ActivityBase {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         switch (id) {
-            case R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
+            /*case R.id.home:
+                this.mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;*/
             case R.id.actionSearch:
                 return true;
         }
@@ -43,15 +47,17 @@ public class ActivityMain extends ActivityBase {
 
     @Override
     protected void retrieveItems() {
-        this.mToolbar = (Toolbar) this.findViewById(R.id.toolbar);
         this.mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        this.mToolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        this.mNavigationView = (NavigationView) this.findViewById(R.id.navigationView);
     }
 
     @Override
     protected void mapInformationToItems() {
         this.setSupportActionBar(this.mToolbar);
-        ActionBar actionBar = this.getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.drawable.ic__drawer);
+        this.setUpNavigationDrawer();
+        //ActionBar actionBar = this.getSupportActionBar();
+        //actionBar.setHomeAsUpIndicator(R.drawable.ic__drawer);
     }
 
     @Override
@@ -62,5 +68,49 @@ public class ActivityMain extends ActivityBase {
     @Override
     protected int getScreenContainerViewId() {
         return R.id.container;
+    }
+
+    private void setUpNavigationDrawer() {
+        this.mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem pMenuItem) {
+                ActivityMain.this.mDrawerLayout.closeDrawers();
+                pMenuItem.setChecked(true);
+
+                switch (pMenuItem.getItemId()){
+                    case R.id.drawer_item__home:
+                        return true;
+                    case R.id.drawer_item__sounds:
+                        return true;
+                    case R.id.drawer_item__playlists:
+                        return true;
+                    case R.id.drawer_item__favorites:
+                        return true;
+                    default:
+                        return true;
+
+                }
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                this.mDrawerLayout,
+                this.mToolbar,
+                R.string.desc__navigation_drawer_open,
+                R.string.desc__navigation_drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        this.mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 }
